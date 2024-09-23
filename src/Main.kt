52f0051b92b0bb3f73eb55e67a7702a8297a405a
Admin <reteamboss@gmail.com>
2024-09-23 17:51:21 +0300

    4

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    println("Hello World!")
+    var array: ArrayList<Int> = arrayListOf(0,6,0,0,2,0,0,0,1,0,5,0)
+
+    for (i in array.indices){
+        for (j in 0..array.indices.last - 1){
+            if (array[j] == 0){
+                var temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+        }
+    }
+    println(array)
 }
